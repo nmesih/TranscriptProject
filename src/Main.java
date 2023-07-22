0385492333850a15fef004961357f4e36b44874f
Mesih Alan <nmesihalan@gmail.com>
2023-07-22 20:50:22 +0300

    README.md added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import main.CourseGrade;
+import main.Transcript;
+import util.Grade;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CourseGrade courseGradeComp = new CourseGrade("COMP", 404, 4, Grade.A);
+        CourseGrade courseGradeEce = new CourseGrade("ECE", 101, 4, Grade.C);
+        CourseGrade courseGradeMath = new CourseGrade("MATH", 305, 3, Grade.B);
+        CourseGrade courseGradeCeng = new CourseGrade("CENG", 201, 4, Grade.A);
+        CourseGrade courseGradeMe = new CourseGrade("ME", 509, 3, Grade.D);
+
+
+        Transcript transcript = new Transcript(1112234);
+        transcript.addCourseTaken(courseGradeComp);
+        transcript.addCourseTaken(courseGradeEce);
+        transcript.addCourseTaken(courseGradeMath);
+        transcript.addCourseTaken(courseGradeCeng);
+        transcript.addCourseTaken(courseGradeMe);
+        System.out.println(transcript);
     }
 }
